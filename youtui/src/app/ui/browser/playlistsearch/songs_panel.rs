dc95abb359651a982ade621a6085e582af760f7a
beto bettini <caos_obliquo@proton.me>
2026-09-16 21:32:27 -0300

    feat(queue): context-aware o-menu says Queue for local adds

--- youtui/src/app/ui/browser/playlistsearch/songs_panel.rs
+++ youtui/src/app/ui/browser/playlistsearch/songs_panel.rs
@@ -76,8 +76,8 @@ impl Action for BrowserPlaylistSongsAction {
         match &self {
             BrowserPlaylistSongsAction::PlaySong => "Play song",
             BrowserPlaylistSongsAction::PlaySongs => "Play songs",
-            BrowserPlaylistSongsAction::AddSongToPlaylist => "Add song to playlist",
-            BrowserPlaylistSongsAction::AddSongsToPlaylist => "Add songs to playlist",
+            BrowserPlaylistSongsAction::AddSongToPlaylist => "Add song to queue",
+            BrowserPlaylistSongsAction::AddSongsToPlaylist => "Add songs to queue",
             BrowserPlaylistSongsAction::Sort => "Sort",
             BrowserPlaylistSongsAction::Filter => "Filter",
             BrowserPlaylistSongsAction::ViewLyrics => "View Lyrics",
